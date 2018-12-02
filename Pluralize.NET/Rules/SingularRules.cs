3aa983467859ddf9c1e6441dd08c01283aff07a3
Dennis Prazak <prazak.dennis@gmail.com>
2018-12-02 12:32:12 +0100

    Implement rules & tests from the original library
    - implemented changes to rules and tests according to
    blakeembrey/pluralize diff 08e11dd...298eb67
    
    https://github.com/blakeembrey/pluralize/compare/08e11ddc21211bba4c7dfce77176d0b8a25ac635...298eb67f19369dd30207c0817eda4f0045c6c1a7

--- Pluralize.NET/Rules/SingularRules.cs
+++ Pluralize.NET/Rules/SingularRules.cs
@@ -11,19 +11,17 @@ namespace Pluralize.NET.Rules
             {
                {new Regex("s$",RegexOptions.IgnoreCase), ""},
                {new Regex("(ss)$",RegexOptions.IgnoreCase), "$1"},
-               {new Regex("((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)(?:sis|ses)$",RegexOptions.IgnoreCase), "$1sis"},
-               {new Regex("(^analy)(?:sis|ses)$",RegexOptions.IgnoreCase), "$1sis"},
                {new Regex("(wi|kni|(?:after|half|high|low|mid|non|night|[^\\w]|^)li)ves$",RegexOptions.IgnoreCase), "$1fe"},
                {new Regex("(ar|(?:wo|[ae])l|[eo][ao])ves$",RegexOptions.IgnoreCase), "$1f"},
                {new Regex("ies$",RegexOptions.IgnoreCase), "y"},
                {new Regex("\\b([pl]|zomb|(?:neck|cross)?t|coll|faer|food|gen|goon|group|lass|talk|goal|cut)ies$",RegexOptions.IgnoreCase), "$1ie"},
                {new Regex("\\b(mon|smil)ies$",RegexOptions.IgnoreCase), "$1ey"},
-               {new Regex("(m|l)ice$",RegexOptions.IgnoreCase), "$1ouse"},
+               {new Regex("\\b((?:tit)?m|l)ice$",RegexOptions.IgnoreCase), "$1ouse"},
                {new Regex("(seraph|cherub)im$",RegexOptions.IgnoreCase), "$1"},
-               {new Regex("(x|ch|ss|sh|zz|tto|go|cho|alias|[^aou]us|tlas|gas|(?:her|at|gr)o|ris)(?:es)?$",RegexOptions.IgnoreCase), "$1"},
-               {new Regex("(e[mn]u)s?$",RegexOptions.IgnoreCase), "$1"},
-               {new Regex("(movie|twelve)s$",RegexOptions.IgnoreCase), "$1"},
-               {new Regex("(cris|test|diagnos)(?:is|es)$",RegexOptions.IgnoreCase), "$1is"},
+               {new Regex("(x|ch|ss|sh|zz|tto|go|cho|alias|[^aou]us|t[lm]as|gas|(?:her|at|gr)o|ris)(?:es)?$",RegexOptions.IgnoreCase), "$1"},
+               {new Regex("(analy|ba|diagno|parenthe|progno|synop|the|empha|cri)(?:sis|ses)$",RegexOptions.IgnoreCase), "$1sis"},
+               {new Regex("(movie|twelve|abuse|e[mn]u)s$",RegexOptions.IgnoreCase), "$1"},
+               {new Regex("(test)(?:is|es)$",RegexOptions.IgnoreCase), "$1is"},
                {new Regex("(alumn|syllab|octop|vir|radi|nucle|fung|cact|stimul|termin|bacill|foc|uter|loc|strat)(?:us|i)$",RegexOptions.IgnoreCase), "$1us"},
                {new Regex("(agend|addend|millenni|dat|extrem|bacteri|desiderat|strat|candelabr|errat|ov|symposi|curricul|quor)a$",RegexOptions.IgnoreCase), "$1um"},
                {new Regex("(apheli|hyperbat|periheli|asyndet|noumen|phenomen|criteri|organ|prolegomen|hedr|automat)a$",RegexOptions.IgnoreCase), "$1on"},
@@ -35,13 +33,13 @@ namespace Pluralize.NET.Rules
                {new Regex("(eau)x?$",RegexOptions.IgnoreCase), "$1"},
                {new Regex("men$",RegexOptions.IgnoreCase), "man" },
 
-                {new Regex("pox$",RegexOptions.IgnoreCase),        "$0"},
-                {new Regex("ois$",RegexOptions.IgnoreCase),        "$0"},
+                {new Regex("[^aeiou]ese$",RegexOptions.IgnoreCase),"$0"},
                 {new Regex("deer$",RegexOptions.IgnoreCase),       "$0"},
                 {new Regex("fish$",RegexOptions.IgnoreCase),       "$0"},
-                {new Regex("sheep$",RegexOptions.IgnoreCase),      "$0"},
                 {new Regex("measles$/",RegexOptions.IgnoreCase),   "$0"},
-                {new Regex("[^aeiou]ese$",RegexOptions.IgnoreCase),"$0"}
+                {new Regex("o[iu]s$",RegexOptions.IgnoreCase),     "$0"},
+                {new Regex("pox$",RegexOptions.IgnoreCase),        "$0"},
+                {new Regex("sheep$",RegexOptions.IgnoreCase),      "$0"}
             };
         }
     }
