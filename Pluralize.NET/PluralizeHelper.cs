92bfc5a1306e9c265d9e2b7439466e212fb7742f
Thomas Volden <tv@chargetime.eu>
2022-03-15 10:49:54 +0100

    Added a static helper class as a thin wrapper

---
+++ Pluralize.NET/PluralizeHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Pluralize.NET
+{
+    public static class PluralizeHelper
+    {
+        public static string Pluralize(this int count, string unit)
+        {
+            return new Pluralizer().Format(unit, count, true);
+        }
+
+        public static string Pluralize(this float count, string unit)
+        {
+            return new Pluralizer().Format(unit, (int)count, true);
+        }
+
+        public static string Pluralize(this double count, string unit)
+        {
+            return new Pluralizer().Format(unit, (int)count, true);
+        }
+
+        public static string Pluralize(this string unit, int count)
+        {
+            return new Pluralizer().Format(unit, count);
+        }
+    }
+}
